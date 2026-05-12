66c6c1fb19611182ddafde8e5ef4bc1d915fcf03
xzd <1105367465@qq.com>
2026-05-12 14:45:39 +0800

    更新文档，添加延时函数

--- Core/BSP/bsp/Inc/bsp.h
+++ Core/BSP/bsp/Inc/bsp.h
@@ -8,4 +8,16 @@
  */
 void BSP_Init(void);
 
+/**
+ * @brief 微秒级延时
+ * @param us 延时时长
+ */
+void delay_us(uint32_t us);
+
+/**
+ * @brief 毫秒级延时
+ * @param ms 延时时长
+ */
+void delay_ms(uint32_t ms);
+
 #endif /* __BSP_H */
--- Core/BSP/bsp/bsp.c
+++ Core/BSP/bsp/bsp.c
@@ -11,3 +11,40 @@ void BSP_Init(void)
     
     /* 这里可以添加其他外设的初始化，如 LCD, Key, LED 等 */
 }
+
+/**
+ * @brief 微秒级延时
+ * @note  使用 HAL_GetTick 和系统频率计算，或者简单的循环。
+ *        对于 F407 (168MHz)，使用循环是最简单直接的方法。
+ */
+void delay_us(uint32_t us)
+{
+    uint32_t ticks;
+    uint32_t told, tnow, tcnt = 0;
+    uint32_t reload = SysTick->LOAD;
+
+    ticks = us * (SystemCoreClock / 1000000);
+    told = SysTick->VAL;
+    while (1)
+    {
+        tnow = SysTick->VAL;
+        if (tnow != told)
+        {
+            if (tnow < told)
+                tcnt += told - tnow;
+            else
+                tcnt += reload - tnow + told;
+            told = tnow;
+            if (tcnt >= ticks)
+                break;
+        }
+    }
+}
+
+/**
+ * @brief 毫秒级延时
+ */
+void delay_ms(uint32_t ms)
+{
+    HAL_Delay(ms);
+}
